efe1561708fd3c58cdc013d6f53ebd99371589eb
Cyrus Goudarzi <cgoudarzi@gmail.com>
2020-12-11 23:15:23 -0500

    hr addition

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function Home() {
             </Row>
             <Row>
             <Col>
-              <Bio description="I&#39;m a recent graduate with a MS in Computer Science and Information Systems from the University of North Carolina at Wilmington. 
+              <Bio color="#f7f7f7" description="I&#39;m a recent graduate with a MS in Computer Science and Information Systems from the University of North Carolina at Wilmington. 
               I worked as a Graduate Assistant throughout graduate school under both the Cameron School of Business and the Watson College of Education.  I have been pursuing freelance web development work  
               throughout grad school as well.  I recently defended my thesis project which is focused on text data analytics and visualization in a disaster context.  In my thesis project I was working with natural language processing using Python and Pandas while handling front-end visualizations with Tableau.  
               I am currently seeking full-time employment in Software Engineer or Data Scientist roles." list="HTML, CSS, JavaScript/JQuery, Bootstrap, SQL, Python, R"/>
--- src/Bio.js
+++ src/Bio.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 function Bio(props) {
+  const cl = <hr style={{color: props.color, backgroundColor: props.color, height: 1}}/>;
   const description = <p className="bio">{props.description}</p>;
   const list = <p className="bio">Skills include: {props.list}</p>;
   return (
-  	[description, list]
+  	[cl, description, list]
   	);
 }
 export default Bio;
