9ae730cbd820ea6aeface7a00ee83ef9f03bdfe4
Cyrus Goudarzi <cgoudarzi@gmail.com>
2021-01-05 03:18:53 -0500

    portfolio, web simplify

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ import {
 // import bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Container, Row, Col, Nav, NavDropdown, Image} from 'react-bootstrap';
+import { Container, Row, Col, Nav, Image} from 'react-bootstrap';
 
 // import Components
 import Name from './Name.js';
 import Bio from './Bio.js';
 import Res from './Resume.js';
-import ResPDF from './ResumePDF.js';
+import Port from './Portfolio.js';
 
 // import assetts
 import prof from './gradprof.jpg';
@@ -28,23 +28,19 @@ function App() {
                 <Nav.Item as="li">
                   <Nav.Link className="li" href="/home" >Home</Nav.Link>
                 </Nav.Item>
-                <NavDropdown title="Resume" id="resume">
-                  <NavDropdown.Item href="/resume/html">HTML</NavDropdown.Item>
-                  <NavDropdown.Item href="/resume/pdf">PDF</NavDropdown.Item>
-                </NavDropdown>
-                <NavDropdown title="Portfolio" id="portfolio">
-                  <NavDropdown.Item href="/portfolio/1">Coming Soon</NavDropdown.Item>
-                </NavDropdown>
+                <Nav.Item as="li">
+                  <Nav.Link className="li" href="/resume" >Resume</Nav.Link>
+                </Nav.Item>
+                <Nav.Item as="li">
+                  <Nav.Link className="li" href="/portfolio" >Portfolio</Nav.Link>
+                </Nav.Item>
               </Nav>
               <Switch>
                 <Route path="/portfolio">
-                  <h1>PLACEHOLDER</h1>
+                  <Port/>
                 </Route>
-                <Route path="/resume/html">
+                <Route path="/resume">
                   <Resume/>
-                </Route>
-                <Route path="/resume/pdf">
-                  <ResPDF/>
                 </Route>
                 <Route path="/">
                   <Home/>
@@ -71,7 +67,7 @@ function Home() {
               <Bio color="#f7f7f7" description="I&#39;m a recent graduate with a MS in Computer Science and Information Systems from the University of North Carolina at Wilmington. 
               I worked as a Graduate Assistant throughout graduate school under both the Cameron School of Business and the Watson College of Education.  I have been pursuing freelance web development work  
               throughout grad school as well.  I recently defended my thesis project which is focused on text data analytics and visualization in a disaster context.  In my thesis project I was working with natural language processing using Python and Pandas while handling front-end visualizations with Tableau.  
-              I am currently seeking full-time employment in Software Engineer or Data Scientist roles." list="HTML, CSS, JavaScript/JQuery, Bootstrap, SQL, Python, R"/>
+              I am currently seeking full-time employment in Software Engineer or Data Engineer roles." list="HTML, CSS, JavaScript/JQuery, Bootstrap, SQL, Python, R, Tableau"/>
             </Col>
             </Row>
             </Container>
---
+++ src/Portfolio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Row} from 'react-bootstrap';
+import './App.css';
+
+function Portfolio() {
+
+	return (
+		<Container fluid>
+		<Row>
+		<a href="http://www.therosannahsisters.com" rel="noreferrer" target="_blank" className="portfolio">The Rosannah Sisters</a>
+		</Row>
+		</Container>
+	);
+}
+export default Portfolio;
